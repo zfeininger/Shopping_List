c5e21be1940c99da94e3ba44303a42479723fc1d
zacharyfeininger <zaf83063@uga.edu>
2023-11-29 13:45:59 -0500

    new commit

--- app/src/main/java/edu/uga/cs/shoppinglist/ShoppingListFragment.java
+++ app/src/main/java/edu/uga/cs/shoppinglist/ShoppingListFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class ShoppingListFragment extends Fragment {
+    private EditText editTextItem;
+    private Button button;
 
 
     public ShoppingListFragment() {
@@ -29,7 +35,22 @@ public class ShoppingListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_shopping_list, container, false);
+        editTextItem = v.findViewById(R.id.editText);
+        button = v.findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editTextItem.getText().toString().equals("") || editTextItem.getText().toString().equals(null)) {
+                    return;
+                } else {
+                    Toast.makeText(getContext(), editTextItem.getText().toString() + " has been added to the Shopping List!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        return v;
     }
 }
