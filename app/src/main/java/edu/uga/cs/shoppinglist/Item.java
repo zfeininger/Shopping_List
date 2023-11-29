cc5576375e9a394584479a2bc3db671fae3e1d06
zacharyfeininger <zaf83063@uga.edu>
2023-11-29 14:24:25 -0500

    new file

---
+++ app/src/main/java/edu/uga/cs/shoppinglist/Item.java
@@ -0,0 +1,21 @@
+package edu.uga.cs.shoppinglist;
+
+public class Item {
+    private String itemName;
+
+    public Item() {
+
+    }
+
+    public Item(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+}
