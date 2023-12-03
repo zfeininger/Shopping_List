e9240849a2a596db3235906bf1b51f107183aa34
zacharyfeininger <zaf83063@uga.edu>
2023-12-03 18:07:15 -0500

    working on deleting and updating from purchasedItems page

--- app/src/main/java/edu/uga/cs/shoppinglist/PurchasedItemsAdapter.java
+++ app/src/main/java/edu/uga/cs/shoppinglist/PurchasedItemsAdapter.java
@@ -1,12 +1,24 @@
 package edu.uga.cs.shoppinglist;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -18,14 +30,46 @@ public class PurchasedItemsAdapter extends RecyclerView.Adapter<PurchasedItemsAd
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.basket, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.individual_items, parent, false);
         return new ViewHolder(view);
     }
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         BasketItem basketItem = basketItemList.get(position);
         holder.itemName.setText(basketItem.getItemName());
-        holder.itemPrice.setText(basketItem.getItemPrice());
+        holder.itemPrice.setText("Price: $" + basketItem.getItemPrice());
+
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("previousList/basketItems");
+                DatabaseReference databaseReference1 = FirebaseDatabase.getInstance().getReference("shoppingList");
+                Query query = databaseReference.orderByChild("itemName").equalTo(basketItem.getItemName());
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            BasketItem basketItem = snapshot.getValue(BasketItem.class);
+                            snapshot.getRef().removeValue()
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            Log.d("DELETE", "Item deleted: " + basketItem.getItemName());
+                                            Toast.makeText(view.getContext(), "Item deleted: " + basketItem.getItemName(), Toast.LENGTH_SHORT).show();
+                                            Item item = new Item(basketItem.getItemName().toString());
+                                            databaseReference1.push().setValue(item);
+                                        }
+                                    });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.e("FirebaseDatabase", "Database write cancelled. Details: " + error.getMessage());
+                    }
+                });
+            }
+        });
     }
     @Override
     public int getItemCount() {return basketItemList.size();}
@@ -33,10 +77,16 @@ public class PurchasedItemsAdapter extends RecyclerView.Adapter<PurchasedItemsAd
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView itemName;
         TextView itemPrice;
+        Button deleteButton;
+        Button updateButton;
+        EditText editText;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             itemName = itemView.findViewById(R.id.tvfirstNameBasket);
             itemPrice = itemView.findViewById(R.id.PriceText);
+            deleteButton = itemView.findViewById(R.id.buttonBasket4);
+            updateButton = itemView.findViewById(R.id.button7);
+            editText = itemView.findViewById(R.id.editText);
         }
     }
 }
